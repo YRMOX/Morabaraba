19971249ee4f6e56cb5b449acc4f280cbc97a957
YRMOX <74454927+YRMOX@users.noreply.github.com>
2021-12-17 22:51:51 +0100

    Save Update

--- src/SDL_Game/Morabaraba/Morabaraba_Save.c
+++ src/SDL_Game/Morabaraba/Morabaraba_Save.c
@@ -10,6 +10,12 @@ Morabaraba* LoadMorabaraba(SDL_Renderer* renderer, char* currentDir){
     int size = (int)fgetc(save);
     int playerNumber = (int)fgetc(save);
     Morabaraba* morabaraba = CreateMorabaraba(renderer, size, playerNumber);
+    for(int i=0; i<playerNumber; i++){
+        morabaraba->players[i]->isKiller = (int)fgetc(save);
+        morabaraba->players[i]->id = (int)fgetc(save);
+        morabaraba->players[i]->cowInHand = (int)fgetc(save);
+        morabaraba->players[i]->cowTotalNumber = (int)fgetc(save);
+    }
     for(int i=0; i<size; i++){
         for(int j=0; j<size; j++){
             if(morabaraba->array[i][j] != NULL){
@@ -29,6 +35,12 @@ void SaveMorabaraba(Morabaraba* morabaraba, char* currentDir){
     FILE* save = fopen(path, "wb+");
     fprintf(save, "%c", (char)morabaraba->size);
     fprintf(save, "%c", (char)2);
+    for(int i=0; i<2; i++){
+        fprintf(save, "%c", (char)morabaraba->players[i]->isKiller);
+        fprintf(save, "%c", (char)morabaraba->players[i]->id);
+        fprintf(save, "%c", (char)morabaraba->players[i]->cowInHand);
+        fprintf(save, "%c", (char)morabaraba->players[i]->cowTotalNumber);
+    }
     for(int i=0; i<morabaraba->size; i++){
         for(int j=0; j<morabaraba->size; j++){
             if(morabaraba->array[i][j] != NULL){
